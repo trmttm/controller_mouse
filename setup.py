1656f5fd2e2cbd539b7db3f9613ae01e66b19b61
trmttm <96355189+trmttm@users.noreply.github.com>
2022-01-14 14:30:11 +0400

    Added files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='controller_mouse',
+    version='1.0.0',
+    packages=['controller_mouse'],
+    url='',
+    license='',
+    author='Taro Yamaka',
+    author_email='',
+    description=''
+)
